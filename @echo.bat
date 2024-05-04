3d5efc8730f97903302924b56c6c94991aa1de59
Rkattappa <128559972+Rkattappa@users.noreply.github.com>
2024-05-04 14:50:53 -0700

    Delete @echo.bat

--- @echo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Navigate to the directory where Python is installed
-cd C:\Users\kattappa\AppData\Local\Programs\Python\Python312
-
-REM Run the Python script
-python "C:\Users\kattappa\Desktop\mouse_and_scroll.py"  
-pause
