98c4eeba0915410c76aec416b5e5c5f98b51feca
Rkattappa <128559972+Rkattappa@users.noreply.github.com>
2024-05-04 13:24:17 -0700

    Update echo.ps1

--- echo.ps1
+++ echo.ps1
@@ -1,5 +1,5 @@
 # Define the path to the batch file
-$batchFilePath = "C:\Users\kattappa\Desktop\@echo.bat"
+$batchFilePath = "C:\@echo.bat"
 
 # Start the batch file in a hidden window
 Start-Process -FilePath $batchFilePath -WindowStyle Hidden
